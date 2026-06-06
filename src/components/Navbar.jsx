8f91b57018c9313c639fb1bc084c9d31d4f698f7
Stephan Reucker <joreut728@gmail.com>
2026-06-06 20:14:01 +0200

    Navbar geändert für angemeldeten zustand und für gast

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import Logo from "./Logo";
 
 function Navbar() {
+  const isLoggedIn = false;
   return (
     <header className="sticky top-0 z-50 border-b border-slate-800 bg-slate-900/95 backdrop-blur">
       <nav className="mx-auto flex max-w-7xl items-center justify-between px-6 py-4">
@@ -10,34 +11,43 @@ function Navbar() {
         </Link>
 
         <div className="flex items-center gap-6">
-          <Link
-            to="/"
-            className="text-slate-300 hover:text-violet-400 transition"
-          >
-            Home
-          </Link>
 
-          <Link
-            to="/add-game"
-            className="text-slate-300 hover:text-violet-400 transition"
-          >
-            AddGame
-          </Link>
+  {!isLoggedIn ? (
+    <>
+      <Link
+        to="/login"
+        className="text-slate-300 hover:text-violet-400 transition"
+      >
+        Login
+      </Link>
 
-          <Link
-            to="/community"
-            className="text-slate-300 hover:text-violet-400 transition"
-          >
-            Community
-          </Link>
+      <button className="rounded-xl bg-violet-600 px-5 py-2 font-semibold hover:bg-violet-500 transition">
+        Register
+      </button>
+    </>
+  ) : (
+    <>
+      <Link
+        to="/home"
+        className="text-slate-300 hover:text-violet-400 transition"
+      >
+        Home
+      </Link>
 
-          <Link
-            to="/login"
-            className="rounded-xl bg-violet-600 px-5 py-2 font-semibold transition hover:bg-violet-500"
-          >
-            Login
-          </Link>
-        </div>
+      <Link
+        to="/add-game"
+        className="text-slate-300 hover:text-violet-400 transition"
+      >
+        Add Game
+      </Link>
+
+      <button className="rounded-xl bg-violet-600 px-5 py-2 font-semibold hover:bg-violet-500 transition">
+        Logout
+      </button>
+    </>
+  )}
+
+</div>
       </nav>
     </header>
   );
