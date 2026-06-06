5213f0ae188b0d37d3bacdae5f65de7db7be2501
Stephan Reucker <joreut728@gmail.com>
2026-06-06 20:08:30 +0200

    LandingPage erstellt mit app.jsx verbunden

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,20 @@ import GameDetailPage from "./pages/GameDetailPage";
 import AddGamePage from "./pages/AddGamePage";
 import Navbar from "./components/Navbar";
 import CommunityPage from "./pages/CommunityPage";
+import LandingPage from "./pages/LandingPage";
 
 function App() {
   return (
     <BrowserRouter>
       <main className="min-h-screen bg-slate-950 text-white">
-        
-            <Navbar />
+        <Navbar />
 
         <Routes>
           <Route path="/login" element={<LoginPage />} />
-          <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<LandingPage />} />
+
+          <Route path="/home" element={<HomePage />} />
+
           <Route path="/game/:id" element={<GameDetailPage />} />
           <Route path="/add-game" element={<AddGamePage />} />
           <Route path="/community" element={<CommunityPage />} />
---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,25 @@
+function LandingPage() {
+  return (
+    <section className="mx-auto max-w-6xl px-6 py-20">
+
+      <div className="text-center">
+
+        <h1 className="text-6xl font-bold">
+          Build Your Ultimate
+          <span className="block text-violet-400">
+            Gaming Library
+          </span>
+        </h1>
+
+        <p className="mx-auto mt-6 max-w-3xl text-xl text-slate-400">
+          Track your backlog, rate your favorite games
+          and organize your entire gaming collection.
+        </p>
+
+      </div>
+
+    </section>
+  );
+}
+
+export default LandingPage;
