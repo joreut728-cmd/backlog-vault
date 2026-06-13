74be7c77872a986a100dc25f007f5197cde558bb
Stephan Reucker <joreut728@gmail.com>
2026-06-13 07:17:15 +0200

    firestore hinzugefügt

--- src/data/firebase.js
+++ src/data/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "DEIN_KEY",
-  authDomain: "DEIN_DOMAIN",
-  projectId: "DEIN_ID",
-  storageBucket: "DEIN_BUCKET",
-  messagingSenderId: "DEIN_SENDER",
-  appId: "DEIN_APP_ID",
+  apiKey: "AIzaSyACu3nTjzuj6hwUnEXg65UM_A9GL1aZz6E",
+  authDomain: "backlog-vault.firebaseapp.com",
+  projectId: "backlog-vault",
+  storageBucket: "backlog-vault.firebasestorage.app",
+  messagingSenderId: "355520257238",
+  appId: "1:355520257238:web:24383f6b6feb8709237d77",
 };
 
 const app = initializeApp(firebaseConfig);
